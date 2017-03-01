58f5937e8c736720e09b6c39950883aa84f6a8da
Edge <munish.kapoor@bizedge.com>
2017-03-01 10:07:52 +0800

    build 1.0.1

--- app/src/main/java/com/manishkprsimpleboilerplate/MainActivity.java
+++ app/src/main/java/com/manishkprsimpleboilerplate/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.manishkprboilerplate.utils.NetworkUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
