7f60aa57d9430aa7ee303346e9cc045cd499dfd7
Edge <munish.kapoor@bizedge.com>
2017-03-27 14:29:00 +0800

    Fragment back stack

---
+++ manishkprboilerplate/src/main/java/com/manishkprboilerplate/utils/HostFragment.java
@@ -0,0 +1,38 @@
+package com.manishkprboilerplate.utils;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.manishkprboilerplate.R;
+
+
+public class HostFragment extends BackStackFragment {
+    private Fragment fragment;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.host_fragment, container, false);
+        if (fragment != null) {
+            replaceFragment(fragment, false);
+        }
+        return view;
+    }
+
+    public void replaceFragment(Fragment fragment, boolean addToBackstack) {
+        if (addToBackstack) {
+            getChildFragmentManager().beginTransaction().replace(R.id.hosted_fragment, fragment).addToBackStack(null).commit();
+        } else {
+            getChildFragmentManager().beginTransaction().replace(R.id.hosted_fragment, fragment).commit();
+        }
+    }
+
+    public static HostFragment newInstance(Fragment fragment) {
+        HostFragment hostFragment = new HostFragment();
+        hostFragment.fragment = fragment;
+        return hostFragment;
+    }
+}
