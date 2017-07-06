bbca47cc582fe10d0a9708a931101bfce5f6bb9e
Munish Kapoor <manishkpr@gmail.com>
2017-07-07 02:13:21 +0800

    updated socket timeout

--- manishkprboilerplate/src/main/java/com/manishkprboilerplate/web_services/ClientOkHttp.java
+++ manishkprboilerplate/src/main/java/com/manishkprboilerplate/web_services/ClientOkHttp.java
@@ -1,5 +1,7 @@
 package com.manishkprboilerplate.web_services;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
@@ -9,9 +11,11 @@ import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ClientOkHttp {
 
+    static final int TIME_OUT   =         120;
+
     public static OkHttpClient getOKHTTPClient(){
 
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder().readTimeout(TIME_OUT, TimeUnit.SECONDS).connectTimeout(TIME_OUT, TimeUnit.SECONDS);
 
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
         logging.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -20,6 +24,7 @@ public class ClientOkHttp {
         httpClient.addInterceptor(new LoggingInterceptor());
 
         OkHttpClient client = httpClient.build();
+
 
         return client;
 
