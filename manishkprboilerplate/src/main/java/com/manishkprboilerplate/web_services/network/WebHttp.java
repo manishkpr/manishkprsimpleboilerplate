178e22158c8c1d2e70dc529c41229b761d2c4ea6
Munish Kapoor <manishkpr@gmail.com>
2017-09-08 15:15:24 +0800

    addded post web http
    addded post web http

--- manishkprboilerplate/src/main/java/com/manishkprboilerplate/web_services/network/WebHttp.java
+++ manishkprboilerplate/src/main/java/com/manishkprboilerplate/web_services/network/WebHttp.java
@@ -4,12 +4,19 @@ package com.manishkprboilerplate.web_services.network;
 
 import com.manishkprboilerplate.models.HttpResponse;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class WebHttp {
@@ -74,6 +81,92 @@ public class WebHttp {
 
         return new HttpResponse(statusCode,response.toString());
 
+    }
+
+    public static HttpResponse postMethod(String urlAddress,HashMap<String, String> postDataParams){
+
+
+        URL url;
+        HttpURLConnection conn=null;
+        InputStream inputStream = null;
+        InputStreamReader reader = null;
+        StringBuilder response = new StringBuilder("");
+        int statusCode = 0;
+        try {
+            url = new URL(urlAddress);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(READ_INTERVAL_TIMEOUT); //maximum time to wait for an input stream read
+            conn.setConnectTimeout(CONNECTION_TIMEOUT); //maximum time to wait while connecting
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true); //whether this URLConnection allows receiving data
+            conn.setDoOutput(true);
+            conn.connect();
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+
+            int status = conn.getResponseCode();
+            statusCode = status;
+            if(status == 200) {
+                inputStream = conn.getInputStream();
+            }
+            else {
+                inputStream = conn.getErrorStream();
+            }
+
+            reader = new InputStreamReader(inputStream);
+
+
+            int data = reader.read();
+            while(data!=-1){
+                response.append( (char)data );
+                data = reader.read();
+            }
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+
+            try {
+                if(reader!=null) reader.close();
+                if(inputStream!=null) inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(conn!=null) conn.disconnect();
+
+        }
+
+        return new HttpResponse(statusCode,response.toString());
+
+    }
+
+    public static String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
     }
 
 }
